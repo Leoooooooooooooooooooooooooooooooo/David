4bb286abe2537e713a3cfee33a50d0f6fc408fa0
dylsheridan766 <dylsheridan@gmail.com>
2026-06-12 00:23:22 -0400

    added gambling

---
+++ src/commands/gamble.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { getOrCreateUser, addMoney, setSick, hungerLoss } from '../db/index';
+
+
+
+export const data = new SlashCommandBuilder()
+  .setName('gamble')
+  .setDescription('Gabmle your lifes savings away');
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+  const userId = interaction.user.id;
+  const guildId = interaction.guildId!;
+
+  const user = await getOrCreateUser(userId, guildId);
+user.money
+
+
+
+const earned = Math.floor(Math.random() * ((user.money * 2) + 1)); //between 0 and double all of their money
+
+addMoney(userId, -user.money); 
+addMoney(userId, earned);
+ 
+}
