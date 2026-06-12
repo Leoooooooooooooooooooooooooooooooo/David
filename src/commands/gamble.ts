6d828662d4c6ef929e0c06384f1bd966657b3c44
Leo <leo.saracino.q@gmail.com>
2026-06-12 00:41:29 -0400

    try/catch

--- src/commands/gamble.ts
+++ src/commands/gamble.ts
@@ -5,20 +5,22 @@ import { getOrCreateUser, addMoney} from '../db/index';
 
 export const data = new SlashCommandBuilder()
   .setName('gamble')
-  .setDescription('Gabmle your lifes savings away');
+  .setDescription('Gamble your lifes savings away');
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-  const userId = interaction.user.id;
-  const guildId = interaction.guildId!;
+  try {
+    const userId = interaction.user.id;
+    const guildId = interaction.guildId!;
 
-  const user = await getOrCreateUser(userId, guildId);
+    const user = await getOrCreateUser(userId, guildId);
 
+    const earned = Math.floor(Math.random() * ((user.money * 2) + 1));
 
-
-const earned = Math.floor(Math.random() * ((user.money * 2) + 1)); //between 0 and double all of their money
-
-await addMoney(userId, -user.money); 
-await addMoney(userId, earned);
-await interaction.reply(`🎰 **${interaction.user.displayName}** gambled and got **$${earned}**`);
-
+    await addMoney(userId, -user.money);
+    await addMoney(userId, earned);
+    await interaction.reply(`🎰 **${interaction.user.displayName}** gambled and got **$${earned}**`);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply('Something went wrong!');
+  }
 }
